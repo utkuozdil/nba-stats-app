067b2a50e6a9667449cec1f76d723c4b7250d941
utkuozdil <utkuozdil@gmail.com>
2024-12-19 17:27:25 +0300

    Adding NBA API as source

---
+++ src/handlers/save_daily_results_nba_api.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+
+from src.integrations.nbacom import NbaCom
+from src.services.s3 import S3
+from src.util.config import BUCKET_NAME
+
+nbaAPI = NbaCom()
+s3 = S3(BUCKET_NAME)
+
+
+def handler(_event, _context):
+    try:
+        yesterday = datetime.now() - timedelta(days=1)
+        yesterday_iso_format = yesterday.strftime('%Y-%m-%d')
+        games = nbaAPI.get_games(date=yesterday_iso_format)
+        s3.upload_to_bucket(data=games, key=f"{yesterday_iso_format}/nba-api.json")
+    except Exception as e:
+        print(f"Error handling request: {str(e)}")
+
---
+++ src/integrations/nbacom.py
@@ -0,0 +1,15 @@
+import json
+
+from nba_api.stats.endpoints import scoreboardv2
+
+
+class NbaCom:
+
+    def __init__(self):
+        self.scoreboardAPI = scoreboardv2
+
+    def get_games(self, date: str):
+        games = self.scoreboardAPI.ScoreboardV2(game_date=date)
+        raw_json = games.get_json()
+        formatted_json = json.loads(raw_json)
+        return formatted_json
