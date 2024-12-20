93fb435d485a32531da473090f6f1d2f53047bed
utkuozdil <utkuozdil@gmail.com>
2024-12-20 16:47:46 +0300

    fixes

--- src/handlers/process_nba_api_game_data.py
+++ src/handlers/process_nba_api_game_data.py
@@ -4,6 +4,7 @@ from src.services.dynamodb import DynamoDB
 from src.services.s3 import S3
 from src.util.bdl_data_extractor import BDLDataExtractor
 from src.util.config import BUCKET_NAME, GAME_TABLE_NAME
+from src.util.nba_api_data_extractor import NBAAPIDataExtractor
 
 s3 = S3(BUCKET_NAME)
 dynamodb = DynamoDB(GAME_TABLE_NAME)
@@ -15,8 +16,8 @@ def handler(event, context):
             sqs_message = json.loads(message_body["Message"])
             s3_key = sqs_message["s3_key"]
             raw_data = s3.retrieve_data_from_bucket(key=s3_key)
-            bdl_data_extractor = BDLDataExtractor(data=raw_data)
-            extracted_games = bdl_data_extractor.extract_game_data()
+            nba_api_data_extractor = NBAAPIDataExtractor(data=raw_data)
+            extracted_games = nba_api_data_extractor.extract_game_data()
             dynamodb.save_batch(data=extracted_games)
         except Exception as e:
             print(f"Error processing message: {str(e)}")
