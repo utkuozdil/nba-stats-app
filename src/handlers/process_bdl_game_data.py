c8e97beacd0eab3f76a3f377f40bde172996bc04
utkuozdil <utkuozdil@gmail.com>
2024-12-22 16:19:03 +0300

    refactor

--- src/handlers/process_bdl_game_data.py
+++ src/handlers/process_bdl_game_data.py
@@ -5,6 +5,7 @@ from src.services.s3 import S3
 from src.services.sqs import SQS
 from src.util.bdl_data_extractor import BDLDataExtractor
 from src.util.config import BUCKET_NAME, GAME_TABLE_NAME, NORMALIZE_GAME_QUEUE_URL, GAME_TABLE_INDEX
+from src.util.process_data_util import filter_games, generate_game_id
 
 s3 = S3(BUCKET_NAME)
 dynamodb = DynamoDB(GAME_TABLE_NAME)
@@ -24,27 +25,10 @@ def handler(event, context):
                 date = extracted_games[0].date
                 previously_recorded_games = dynamodb.get_by_date(index_name=GAME_TABLE_INDEX,
                                                                  key="date", date=date)
-                prev_recorded_unified_games = list(
-                    filter(lambda x: x["source"] == "Unified", previously_recorded_games))
-                prev_recorded_unified_game_ids = set([x["game_id"]
-                                                      for x in prev_recorded_unified_games])
-
-                filtered_games = []
-                for extracted_game in extracted_games:
-                    temp_game_id = "_".join([extracted_game.date,
-                                             extracted_game.home_team.abbreviation,
-                                             extracted_game.visitor_team.abbreviation,
-                                             "Unified"])
-                    if temp_game_id not in prev_recorded_unified_game_ids:
-                        filtered_games.append(extracted_game)
-
+                filtered_games = filter_games(extracted_games, previously_recorded_games)
                 dynamodb.save_batch(data=filtered_games)
                 for extracted_game in filtered_games:
-                    game_id = "_".join([extracted_game.date,
-                                        extracted_game.home_team.abbreviation,
-                                        extracted_game.visitor_team.abbreviation,
-                                        "BallDontLie"
-                                        ])
+                    game_id = generate_game_id(extracted_game, "BallDontLie")
                     sqs.send_message(message=game_id)
         except Exception as e:
             print(f"Error processing message: {str(e)}")
--- src/handlers/process_nba_api_game_data.py
+++ src/handlers/process_nba_api_game_data.py
@@ -6,6 +6,7 @@ from src.services.s3 import S3
 from src.services.sqs import SQS
 from src.util.config import BUCKET_NAME, GAME_TABLE_NAME, NORMALIZE_GAME_QUEUE_URL, GAME_TABLE_INDEX
 from src.util.nba_api_data_extractor import NBAAPIDataExtractor
+from src.util.process_data_util import filter_games, generate_game_id
 
 s3 = S3(BUCKET_NAME)
 dynamodb = DynamoDB(GAME_TABLE_NAME)
@@ -25,28 +26,12 @@ def handler(event, context):
                 date = extracted_games[0].date
                 previously_recorded_games = dynamodb.get_by_date(index_name=GAME_TABLE_INDEX,
                                                                  key="date", date=date)
-                prev_recorded_unified_games = list(
-                    filter(lambda x: x["source"] == "Unified", previously_recorded_games))
-                prev_recorded_unified_game_ids = set([x["game_id"]
-                                                      for x in prev_recorded_unified_games])
-
-                filtered_games = []
-                for extracted_game in extracted_games:
-                    temp_game_id = "_".join([extracted_game.date,
-                                        extracted_game.home_team.abbreviation,
-                                        extracted_game.visitor_team.abbreviation,
-                                        "Unified"])
-                    if temp_game_id not in prev_recorded_unified_game_ids:
-                        filtered_games.append(extracted_game)
-
+                filtered_games = filter_games(extracted_games, previously_recorded_games)
                 dynamodb.save_batch(data=filtered_games)
                 for extracted_game in filtered_games:
-                    game_id = "_".join([extracted_game.date,
-                                        extracted_game.home_team.abbreviation,
-                                        extracted_game.visitor_team.abbreviation,
-                                        "NBA_API"
-                                        ])
+                    game_id = generate_game_id(extracted_game, "NBA_API")
                     sqs.send_message(message=game_id)
         except Exception as e:
             traceback.print_exc()
             print(f"Error processing message: {str(e)}")
+
